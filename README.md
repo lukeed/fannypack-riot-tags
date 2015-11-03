40c1c086494352476cf582692afad2eb77562c8d
luke <luke.edwards05@gmail.com>
2015-11-03 01:53:06 -0800

    first commit, push

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # fannypack-riot-tags
-Fannypack task for compiling Riot.js tags
+Official Fannypack task for compiling Riot.js tags
+
+## Installation
+`npm install fannypack-riot-tags --save-dev`
+
+## Usage
+```javascript
+// gulpfile.js
+
+var Fannypack = require('fannypack')
+
+require('fannypack-riot-tags')
+
+Fannypack.Config.tags = {
+  // Config.root.src + 'src' dir
+  src: 'tags',
+  // Config.root.src + 'dest' dir
+  dest: 'javascripts',
+  // Concatenated file name
+  output: '-templates.js',
+  // Auto-run this task in dev mode?
+  watchTask: true,
+  assetTask: true,
+  // File extensions to watch
+  extensions: ['tag']
+}
+
+Fannypack.init()
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp')
+var Fannypack   = require('fannypack')
+var Pather      = Fannypack.$.Pather
+var BrowserSync = Fannypack.$.BrowserSync
+
+// Additional Packages
+var riot        = require('gulp-riot')
+var concat      = require('gulp-concat')
+var insert      = require('gulp-insert')
+
+Fannypack.Tasks['tags'] = function(config){
+  if(!config.tags) return
+
+  var paths = {
+    src: Pather.join(config.root.src, config.tags.src, '/**/*'),
+    dest: Pather.join(config.root.src, config.tags.dest) // dump into src/javascripts
+  }
+
+  gulp.task('tags', function() {
+    return gulp.src(paths.src)
+      .pipe( concat(config.tags.output) )
+      .pipe( riot({compact: true}) )
+      // insert riot dependency declaration
+      .pipe( insert.prepend("import riot from 'riot';\n\n") )
+      .pipe( gulp.dest(paths.dest) )
+      .pipe( BrowserSync.reload({stream:true}) )
+  })
+}
